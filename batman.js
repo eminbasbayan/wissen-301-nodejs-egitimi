7254acc7f0a1f3a8f181ecfd0c5ca758dca6d34a
eminbasbayan <emin.basbayan@icloud.com>
2023-11-29 20:39:38 +0300

    first commit

---
+++ batman.js
@@ -0,0 +1,7 @@
+// (function (exports, require, module, __filename, __dirname) {
+//   const superHero = "Batman";
+//   console.log(superHero);
+// })();
+
+const superHero = "Batman";
+console.log(superHero);
---
+++ index.js
@@ -0,0 +1,32 @@
+const math = require("./add.js");
+
+console.log(math.add(4, 5));
+console.log(math.subtract(7, 5));
+
+let obj1 = { name: "Emin Başbayan" };
+let obj2 = obj1;
+// obj2.name = "Nur Başbayan";
+console.log(obj1.name);
+
+obj2 = { name: "Nur Başbayan" };
+console.log(obj2.name);
+
+// const SuperHero = require("./super-hero.js");
+
+// const superman = new SuperHero("Superman");
+
+// console.log(superman.getName());
+
+// const superHero = require("./super-hero");
+
+// console.log(superHero.getName());
+
+// superHero.setName("Superman");
+
+// console.log(superHero.getName());
+
+// delete require.cache[require.resolve("./super-hero")];
+
+// const newSuperHero = require("./super-hero");
+
+// console.log(newSuperHero.getName());
