311ca650179bc914e7b79628f44bf46c0eb729cf
MustWis <147651626+MustWis@users.noreply.github.com>
2023-11-30 21:40:05 +0300

    streams ve buffer

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-const PizzaShop = require("./pizza-shop");
-const pizzaShop = new PizzaShop();
+const buffer = new Buffer("Emin");
 
-pizzaShop.on("order", (size, topping)=> {
-    console.log(`Bir ${size} ve ${topping} ile pişiriliyor.`);
-})
-
-pizzaShop.order({
-    size: "large",
-    topping: ""
-
-});
-pizzaShop.displayOrderNumber();
-
-
-// const EventEmitter = require("node:events");
-// const emitter = new EventEmitter();
-
-// emitter.on("orderPizza", (size, topping)=>{
-//      console.log(`Bir ${size} ve ${topping} ile pişiriliyor.`);
-// })
-
-// emitter.emit("orderPizza", "large", "mantar");
+console.log(buffer);
+console.log(buffer.toString());
