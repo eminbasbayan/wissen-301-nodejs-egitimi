c6328c455af2b4313ba9bc5dac75ee7e583a11e2
MustWis <147651626+MustWis@users.noreply.github.com>
2023-11-30 21:30:34 +0300

    event modülü uygulama

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const EventEmitter = require("node:events");
-const emitter = new EventEmitter();
+const PizzaShop = require("./pizza-shop");
+const pizzaShop = new PizzaShop();
 
-
-emitter.on("orderPizza", (size, topping)=>{
-     console.log(`Bir ${size} ve ${topping} ile pişiriliyor.`);
+pizzaShop.on("order", (size, topping)=> {
+    console.log(`Bir ${size} ve ${topping} ile pişiriliyor.`);
 })
 
+pizzaShop.order({
+    size: "large",
+    topping: ""
 
-emitter.emit("orderPizza", "large", "mantar");
+});
+pizzaShop.displayOrderNumber();
 
+
+// const EventEmitter = require("node:events");
+// const emitter = new EventEmitter();
+
+// emitter.on("orderPizza", (size, topping)=>{
+//      console.log(`Bir ${size} ve ${topping} ile pişiriliyor.`);
+// })
+
+// emitter.emit("orderPizza", "large", "mantar");
