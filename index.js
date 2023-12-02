31916cd5cd3a09c1033424a809f6aba6fa0beb7d
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-02 11:00:47 +0300

    streams ile html response

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((request, response) => {
-  const html = fs.readFileSync("./index.html", "utf-8");
   response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  response.end(html);
+  fs.createReadStream(__dirname + "/index.html").pipe(response);
 });
 
 server.listen(5000, () => {
