a8fbba34b4142d5893a87e588e90dfb2f75d9ab5
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-06 19:42:28 +0300

    res redirect

--- middleware.js
+++ middleware.js
@@ -9,12 +9,12 @@ app.get("/index.html", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get("/new-page.html", () => {
+app.get("/new-page.html", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.get("/old-page.html", () => {
-  res.sendFile(path.join(__dirname, "views", "old-page.html"));
+app.get("/old-page.html", (req, res) => {
+  res.redirect(301, "/new-page.html")
 });
 
 app.listen(PORT, () => {
