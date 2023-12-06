a1b72a5da9914ba522a8c6aea219eb15bc3f4f1b
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-06 19:38:30 +0300

    html renderlama yolları

--- middleware.js
+++ middleware.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5500;
+
+app.get("/index.html", (req, res) => {
+  //   res.send("Hello World!");
+  // res.sendFile("./views/index.html", {root: __dirname})
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page.html", () => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page.html", () => {
+  res.sendFile(path.join(__dirname, "views", "old-page.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
