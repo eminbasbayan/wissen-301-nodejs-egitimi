4638e223926d8a6cf27fe0fdf0095d22de2b1b16
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-06 20:54:44 +0300

    content-type application/x-www-form-urlendcoded

--- middleware.js
+++ middleware.js
@@ -3,8 +3,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5500;
 
+// content-type application/x-www-form-urlendcoded
+app.use(express.urlencoded({ extended: false }));
+
 // server static files
-app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   //   res.send("Hello World!");
@@ -35,22 +38,22 @@ app.get(
   }
 );
 
-const one = (req, res, next) =>{
-    console.log("one");
-    next()
-}
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
 
-const two = (req, res, next) =>{
-    console.log("two");
-    next()
-}
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
 
-const three = (req, res, next) =>{
-    console.log("three");
-    res.send("finish!");
-}
+const three = (req, res, next) => {
+  console.log("three");
+  res.send("finish!");
+};
 
-app.get("/chain", [one, two, three])
+app.get("/chain", [one, two, three]);
 
 app.all("*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
