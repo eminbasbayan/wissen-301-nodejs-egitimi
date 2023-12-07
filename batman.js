505b7a6a944c70c27a89eb069f1863b80e9d4918
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-07 20:49:10 +0300

    proje sadeleştirme

--- batman.js
+++
@@ -1,7 +0,0 @@
-// (function (exports, require, module, __filename, __dirname) {
-//   const superHero = "Batman";
-//   console.log(superHero);
-// })();
-
-const superHero = "Batman";
-console.log(superHero);
--- main.mjs
+++
@@ -1,4 +0,0 @@
-import add from "./math-esm.mjs";
-
-console.log(add(5, 6));
-// console.log(math.subtract(5, 6));
