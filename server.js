e22f3860eae067129d122df5ede3b5268338a715
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-07 21:31:31 +0300

    route belirleme

--- server.js
+++ server.js
@@ -36,51 +36,7 @@ app.use(express.json());
 // server static files
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  //   res.send("Hello World!");
-  // res.sendFile("./views/index.html", {root: __dirname})
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("/new-page.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page.html", (req, res) => {
-  res.redirect(301, "/new-page.html");
-});
-
-app.get(
-  "/hello.html",
-  (req, res, next) => {
-    console.log("Hello World!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Hi!");
-    next();
-  },
-  (req, res) => {
-    res.send("Finish!");
-  }
-);
-
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res, next) => {
-  console.log("three");
-  res.send("finish!");
-};
-
-app.get("/chain", [one, two, three]);
+app.use("/", require("./routes/route.js"));
 
 app.all("*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
