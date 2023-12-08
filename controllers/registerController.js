1aa994b5943b88c664fa4a05d4516f080e3b52d1
MustWis <147651626+MustWis@users.noreply.github.com>
2023-12-08 21:15:14 +0300

    register işlemi

---
+++ controllers/registerController.js
@@ -0,0 +1,41 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleNewUser = async (req, res) => {
+  const { user: newUsername, pwd } = req.body;
+  //   const user = req.body.user;
+  //   const pwd = req.body.pwd;
+
+  if (!newUsername || !pwd) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required!" });
+  }
+  const duplicate = usersDB.users.find(
+    (person) => person.username === newUsername
+  );
+  if (duplicate) return res.sendStatus(409);
+  try {
+    const newUser = { username: newUsername, password: pwd };
+    usersDB.setUsers([...usersDB.users, newUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+
+    res.status(201).json({ success: `New user ${newUsername} created!` });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  handleNewUser,
+};
